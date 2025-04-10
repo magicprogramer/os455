1bbe4238eab6f682d0c9ddf85cba0a898c004b0e
ahmed ezzat <ahmedalahly800@gmail.com>
2025-04-10 10:38:10 +0200

    php/lab1

---
+++ src/config.php
@@ -0,0 +1,12 @@
+<?php
+function conf(){
+    return [
+        "max_length"=>[
+            "name"=>25,
+            "msg"=>255
+        ],
+        "msg"=> "<strong>thanks for contacting US </strong>"
+    ];
+    }
+        
+?>
