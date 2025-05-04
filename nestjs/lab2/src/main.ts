4ed52f158630836c1df22cd13368008f08393f73
ahmed ezzat <ahmedalahly800@gmail.com>
2025-05-04 22:43:46 +0300

    configure swegger

--- nestjs/lab2/src/main.ts
+++ nestjs/lab2/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('ITI example')
+    .setVersion('1.0')
+    .addTag('ALEX')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
