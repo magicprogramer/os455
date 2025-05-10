67f15386177409b25f6bf5279cd992f2189be64c
ahmed ezzat <ahmedalahly800@gmail.com>
2025-05-10 15:12:52 +0300

    remove categories folder

--- nextjs/lab1/src/app/categories/not-found.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function notFound() {
-  try {
-    throw new Error("category is not found");
-  } catch {
-    return <div>invalid category</div>;
-  }
-}
